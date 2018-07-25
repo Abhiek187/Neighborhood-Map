492d3a520385447f29293f66046a0fbe85746109
Abhiek187 <achaudhuri2011@yahoo.com>
2018-07-24 21:11:56 -0400

    feat: Add map to screen

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import GoogleMapReact from 'google-map-react';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <GoogleMapReact
+          bootstrapURLKeys={{key: 'AIzaSyAgW5OHRMNIfawf6DfY_UpnK1MqtJyN87E'}}
+          defaultCenter={{lat: 40.441815, lng: -74.511608}}
+          defaultZoom={13}
+        ></GoogleMapReact>
       </div>
     );
   }
