82505551c95691ad699420e3c960d6bff289ece1
Abhiek187 <achaudhuri2011@yahoo.com>
2018-07-25 23:14:45 -0400

    feat: Add infowindow to default markers

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
-import { withScriptjs, withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
+import { withScriptjs, withGoogleMap, GoogleMap, Marker, InfoWindow } from 'react-google-maps';
 import './App.css';
 
 class App extends Component {
   render() {
     // Create Map component
     const Map = withScriptjs(withGoogleMap(props => (
-      <GoogleMap defaultCenter={{lat: 40.441815, lng: -74.511608}} defaultZoom={13}>
-        <Marker position={{lat: 40.4288947, lng: -74.5139918}}/>
+      <GoogleMap defaultCenter={{lat: 40.441643, lng: -74.511790}} defaultZoom={13}>
+        <Marker title="Regal Commerce Center Stadium 18" position={{lat: 40.443312, lng: -74.503794}}>
+          <InfoWindow><div>Regal Commerce Center Stadium 18</div></InfoWindow>
+        </Marker>
+        <Marker title="Crystal Springs Family Waterpark" position={{lat: 40.408719, lng: -74.446151}}>
+          <InfoWindow><div>Crystal Springs Family Waterpark</div></InfoWindow>
+        </Marker>
+        <Marker title="Costco Wholesale" position={{lat: 40.436565, lng: -74.507282}}>
+          <InfoWindow><div>Costco Wholesale</div></InfoWindow>
+        </Marker>
+        <Marker title="Rutgers Gardens" position={{lat: 40.473683, lng: -74.423132}}>
+          <InfoWindow><div>Rutgers Gardens</div></InfoWindow>
+        </Marker>
+        <Marker title="Bunker Hill Golf Course" position={{lat: 40.437306, lng: -74.575925}}>
+          <InfoWindow><div>Bunker Hill Golf Course</div></InfoWindow>
+        </Marker>
       </GoogleMap>
     )));
 
