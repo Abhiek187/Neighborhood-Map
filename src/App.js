1148a9a63747dc2c996099349983d81c97a42760
Abhiek187 <achaudhuri2011@yahoo.com>
2018-07-26 21:22:02 -0400

    refactor: Reformat code & adjust markers

--- src/App.js
+++ src/App.js
@@ -1,29 +1,67 @@
-import React, { Component } from 'react';
-import { withScriptjs, withGoogleMap, GoogleMap, Marker, InfoWindow } from 'react-google-maps';
+/* global google */
+import React, {Component} from 'react';
+import {withScriptjs, withGoogleMap, GoogleMap, Marker, InfoWindow} from 'react-google-maps';
 import './App.css';
 
+// Create Map component
+const Map = withScriptjs(withGoogleMap(props => (
+  <GoogleMap defaultCenter={{lat: 40.441643, lng: -74.511790}} defaultZoom={13}>
+    {props.markers.map(marker => (
+      <Marker key={marker.id} title={marker.title} position={marker.position}
+        defaultAnimation={google.maps.Animation.DROP} onClick={() => {props.toggleInfoWindow(marker)}}>
+        {marker.showInfo && <InfoWindow onCloseClick={() => {props.toggleInfoWindow(marker)}}>
+          <div>{marker.title}</div>
+        </InfoWindow>}
+      </Marker>
+    ))}
+  </GoogleMap>
+)));
+
 class App extends Component {
+  state = {
+    // Initial markers
+    markers: [
+      {
+        id: 'regal-commerece-center-stadium-18',
+        title: 'Regal Commerce Center Stadium 18',
+        position: {lat: 40.443312, lng: -74.503794},
+        showInfo: false
+      },
+      {
+        id: 'crystal-springs-family-waterpark',
+        title: 'Crystal Springs Family Waterpark',
+        position: {lat: 40.408711, lng: -74.445983},
+        showInfo: false
+      },
+      {
+        id: 'costco-wholesale',
+        title: 'Costco Wholesale',
+        position: {lat: 40.436565, lng: -74.507282},
+        showInfo: false
+      },
+      {
+        id: 'milltown-ice-cream-depot',
+        title: 'Milltown Ice Cream Depot',
+        position: {lat: 40.453293, lng: -74.434404},
+        showInfo: false
+      },
+      {
+        id: 'pro-skate',
+        title: 'Pro Skate',
+        position: {lat: 40.415439, lng: -74.528226},
+        showInfo: false
+      }
+    ]
+  };
+
+  toggleInfoWindow = marker => {
+    const markers = [...this.state.markers]; // Make a copy of markers
+    markers.filter(m => m.id === marker.id)[0].showInfo = !marker.showInfo;
+    this.setState({markers});
+  };
+
   render() {
-    // Create Map component
-    const Map = withScriptjs(withGoogleMap(props => (
-      <GoogleMap defaultCenter={{lat: 40.441643, lng: -74.511790}} defaultZoom={13}>
-        <Marker title="Regal Commerce Center Stadium 18" position={{lat: 40.443312, lng: -74.503794}}>
-          <InfoWindow><div>Regal Commerce Center Stadium 18</div></InfoWindow>
-        </Marker>
-        <Marker title="Crystal Springs Family Waterpark" position={{lat: 40.408719, lng: -74.446151}}>
-          <InfoWindow><div>Crystal Springs Family Waterpark</div></InfoWindow>
-        </Marker>
-        <Marker title="Costco Wholesale" position={{lat: 40.436565, lng: -74.507282}}>
-          <InfoWindow><div>Costco Wholesale</div></InfoWindow>
-        </Marker>
-        <Marker title="Rutgers Gardens" position={{lat: 40.473683, lng: -74.423132}}>
-          <InfoWindow><div>Rutgers Gardens</div></InfoWindow>
-        </Marker>
-        <Marker title="Bunker Hill Golf Course" position={{lat: 40.437306, lng: -74.575925}}>
-          <InfoWindow><div>Bunker Hill Golf Course</div></InfoWindow>
-        </Marker>
-      </GoogleMap>
-    )));
+    const {markers} = this.state;
 
     return (
       <div className="App">
@@ -31,7 +69,9 @@ class App extends Component {
           googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyAgW5OHRMNIfawf6DfY_UpnK1MqtJyN87E&v=3"
           loadingElement={<div className="map-loading"/>}
           containerElement={<div className="map-container"/>}
-          mapElement={<div className="map"/>}/>
+          mapElement={<div className="map"/>}
+          markers={markers}
+          toggleInfoWindow={this.toggleInfoWindow}/>
       </div>
     );
   }
