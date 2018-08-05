7af3434588c85ff76081e63b4cfbb00bb990241f
Abhiek187 <achaudhuri2011@yahoo.com>
2018-08-05 14:06:19 -0400

    style: Make page more responsive

--- src/ListView.js
+++ src/ListView.js
@@ -23,23 +23,27 @@ class ListView extends Component {
 
 		return (
 			<div className={isHidden ? 'list-view hidden' : 'list-view'}>
-        <input className="list-toggle" type="image" src={menu}
-        	alt={isHidden ? 'Show list view' : 'Hide list view'}
-        	onClick={() => this.setState({isHidden: !isHidden})}/>
-        <h1 className="list-title" tabIndex={0}>Neighborhood Map</h1>
-        <form className="list-form" onSubmit={e => onFilterLocations(e, query)}>
-          <input className="list-search" type="text" placeholder="Search location"
-            onChange={e => this.setState({query: e.target.value})}/>
-          <input className="list-submit" type="submit" value="Search"/>
-        </form>
-        <div className="list-locations">
-          {markers.filter(marker => marker.isVisible).map(marker => (
-            <div key={marker.id}>
-              <button className={marker.showInfo ? 'list-button selected' : 'list-button'} type="button"
-                onClick={() => onToggleInfoWindow(marker)}>{marker.title}</button>
-              <hr/>
-            </div>
-          ))}
+        <div className="list-side">
+          <input className="list-toggle" type="image" src={menu}
+          	alt={isHidden ? 'Show list view' : 'Hide list view'}
+          	onClick={() => this.setState({isHidden: !isHidden})}/>
+        </div>
+        <div className={isHidden ? "list-main hidden" : "list-main"}>
+          <h1 className="list-title" tabIndex={0}>Neighborhood Map</h1>
+          <form className="list-form" onSubmit={e => onFilterLocations(e, query)}>
+            <input className="list-search" type="text" placeholder="Search location"
+              onChange={e => this.setState({query: e.target.value})}/>
+            <input className="list-submit" type="submit" value="Search"/>
+          </form>
+          <div className="list-locations">
+            {markers.filter(marker => marker.isVisible).map(marker => (
+              <div key={marker.id}>
+                <button className={marker.showInfo ? 'list-button selected' : 'list-button'} type="button"
+                  onClick={() => onToggleInfoWindow(marker)}>{marker.title}</button>
+                <hr/>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     );
