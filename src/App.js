356d1146eced86fa46954fb8fd3b24b75c83cbea
Abhiek187 <achaudhuri2011@yahoo.com>
2022-05-07 23:14:30 -0400

    Replace react-google-maps with @react-google-maps/api

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-/* global google */
-import React, { Component } from "react";
-import scriptLoader from "react-async-script-loader";
+import React from "react";
+import { useJsApiLoader } from "@react-google-maps/api";
 import $ from "jquery";
 import "./App.css";
 import ListView from "./ListView";
@@ -15,63 +14,63 @@ const api =
   "Bearer 5gHT0N2H91kvYB8spnGJj0SD4Cub-O1qp35smS1pSrs0BFyGEayFl6W7AZWROPauJ2TU5gOcm2B1Otx" +
   "adbNvCb0hcu_PFngOKC1f5a4QzgI5lR1gt2WeZoBa7zNeW3Yx";
 
-class App extends Component {
-  state = {
-    // Initial markers
-    markers: JSON.parse(JSON.stringify(markerData)),
-    willLoad: true,
-  };
+const App = () => {
+  // Initial markers
+  const [markers, setMarkers] = React.useState(
+    JSON.parse(JSON.stringify(markerData))
+  );
+  const { isLoaded, loadError } = useJsApiLoader({
+    googleMapsApiKey: "AIzaSyAgW5OHRMNIfawf6DfY_UpnK1MqtJyN87E",
+  });
 
-  componentWillReceiveProps({ isScriptLoaded, isScriptLoadSucceed }) {
-    if (isScriptLoaded && !this.props.isScriptLoaded) {
+  React.useEffect(() => {
+    if (isLoaded) {
       // Perform asynchronous functions after Google Maps has loaded successfully
-      if (isScriptLoadSucceed) {
-        const markers = [...this.state.markers]; // Make a copy of markers
-        markers.map((marker) => {
-          // Set common properties of all markers
-          marker.animation = google ? google.maps.Animation.DROP : null;
-          marker.showInfo = false;
-          marker.isVisible = true;
-          // Click focused marker when user hits enter
-          document.addEventListener(
-            "keyup",
-            (event) =>
-              event.target.title === marker.title &&
-              event.key === "Enter" &&
-              this.toggleInfoWindow(marker)
-          );
-          // Get image and reviews from Yelp
-          this.getBusiness(marker)
-            .done((data) => {
-              marker.id = data.businesses[0].id;
-              marker.url = data.businesses[0].url;
-              marker.image = data.businesses[0].image_url;
-              this.getReviews(marker.id)
-                .done((data) => {
-                  marker.reviews = [];
-                  for (const review of data.reviews) {
-                    marker.reviews.push({
-                      id: review.id,
-                      rating: review.rating,
-                      text: review.text,
-                    });
-                  }
-                })
-                .fail((xhr, textStatus, error) =>
-                  console.error(`Error: ${error}`)
-                );
-            })
-            .fail((xhr, textStatus, error) => console.error(`Error: ${error}`));
+      const newMarkers = [...markers]; // Make a copy of markers
+      newMarkers.map((marker) => {
+        // Set common properties of all markers
+        marker.animation = window.google ? google.maps.Animation.DROP : null;
+        marker.showInfo = false;
+        marker.isVisible = true;
+        // Click focused marker when user hits enter
+        document.addEventListener(
+          "keyup",
+          (event) =>
+            event.target.title === marker.title &&
+            event.key === "Enter" &&
+            toggleInfoWindow(marker)
+        );
+        // Get image and reviews from Yelp
+        getBusiness(marker)
+          .done((data) => {
+            marker.id = data.businesses[0].id;
+            marker.url = data.businesses[0].url;
+            marker.image = data.businesses[0].image_url;
+            getReviews(marker.id)
+              .done((data) => {
+                marker.reviews = [];
+                for (const review of data.reviews) {
+                  marker.reviews.push({
+                    id: review.id,
+                    rating: review.rating,
+                    text: review.text,
+                  });
+                }
+              })
+              .fail((xhr, textStatus, error) =>
+                console.error(`Error: ${error}`)
+              );
+          })
+          .fail((xhr, textStatus, error) => console.error(`Error: ${error}`));
 
-          return marker;
-        });
+        return marker;
+      });
 
-        this.setState({ markers }); // Set markers to the clone created above
-      } else this.setState({ willLoad: false });
+      setMarkers(newMarkers); // Set markers to the clone created above
     }
-  }
+  }, []);
 
-  getBusiness = (marker) =>
+  const getBusiness = (marker) =>
     $.ajax({
       // Marker title matches title on Yelp for better search results
       url:
@@ -83,7 +82,7 @@ class App extends Component {
       },
     });
 
-  getReviews = (id) =>
+  const getReviews = (id) =>
     $.ajax({
       url: proxy + `https://api.yelp.com/v3/businesses/${id}/reviews`,
       headers: {
@@ -92,22 +91,22 @@ class App extends Component {
     });
 
   // Control marker behavior and info window when clicked
-  toggleInfoWindow = (marker) => {
-    const markers = [...this.state.markers];
-    marker = markers.filter((m) => m.id === marker.id)[0];
+  const toggleInfoWindow = (marker) => {
+    const newMarkers = [...markers];
+    marker = newMarkers.filter((m) => m.id === marker.id)[0];
     marker.showInfo = !marker.showInfo;
     marker.animation =
-      marker.showInfo && google ? google.maps.Animation.BOUNCE : null;
-    this.setState({ markers });
+      marker.showInfo && window.google ? google.maps.Animation.BOUNCE : null;
+    setMarkers(newMarkers);
   };
 
   // Filter markers based on search query
-  filterLocations = (event, query) => {
+  const filterLocations = (event, query) => {
     event.preventDefault();
-    const markers = [...this.state.markers];
-    markers.map((marker) => {
+    const newMarkers = [...markers];
+    newMarkers.map((marker) => {
       // Marker drops if it becomes visible
-      if (!marker.isVisible && google)
+      if (!marker.isVisible && window.google)
         marker.animation = google.maps.Animation.DROP; // Take a chance to drop before filtering
       // Case-insensitive filter
       marker.isVisible = marker.title
@@ -120,48 +119,42 @@ class App extends Component {
       return marker;
     });
 
-    this.setState({ markers });
+    setMarkers(newMarkers);
   };
 
-  render() {
-    const { isScriptLoadSucceed } = this.props; // Checks whether Google Maps has loaded
-    const { markers, willLoad } = this.state;
-    // Apply focus to all markers
-    const mapElements = [...document.querySelectorAll(".gmnoprint")].slice(
-      0,
-      markers.length
-    );
-    mapElements.map((element) => (element.tabIndex = 0));
+  // Apply focus to all markers
+  const mapElements = [...document.querySelectorAll(".gmnoprint")].slice(
+    0,
+    markers.length
+  );
+  mapElements.map((element) => (element.tabIndex = 0));
 
-    // Placeholder when map doesn't appear
-    const placeholder = willLoad ? (
-      <p className="loading">Loading...</p>
-    ) : (
-      <h1 className="error-message google" tabIndex={0}>
-        Error! Google Maps was unable to load.
-      </h1>
-    );
+  // Placeholder when map doesn't appear
+  const placeholder = loadError ? (
+    <p className="loading">Loading...</p>
+  ) : (
+    <h1 className="error-message google">
+      Error! Google Maps was unable to load.
+    </h1>
+  );
 
-    return (
-      <div className="App">
-        <ListView
+  return (
+    <div className="App">
+      <ListView
+        markers={markers}
+        onToggleInfoWindow={toggleInfoWindow}
+        onFilterLocations={filterLocations}
+      />
+      {isLoaded ? (
+        <NeighborhoodMap
           markers={markers}
-          onToggleInfoWindow={this.toggleInfoWindow}
-          onFilterLocations={this.filterLocations}
+          onToggleInfoWindow={toggleInfoWindow}
         />
-        {isScriptLoadSucceed ? (
-          <NeighborhoodMap
-            markers={markers}
-            onToggleInfoWindow={this.toggleInfoWindow}
-          />
-        ) : (
-          placeholder
-        )}
-      </div>
-    );
-  }
-}
+      ) : (
+        placeholder
+      )}
+    </div>
+  );
+};
 
-export default scriptLoader(
-  "https://maps.googleapis.com/maps/api/js?key=AIzaSyAgW5OHRMNIfawf6DfY_UpnK1MqtJyN87E&v=3"
-)(App);
+export default React.memo(App);
--- src/NeighborhoodMap.js
+++ src/NeighborhoodMap.js
@@ -1,66 +1,165 @@
-import React, {Component} from 'react';
-import {withGoogleMap, GoogleMap, Marker, InfoWindow} from 'react-google-maps';
-import PropTypes from 'prop-types';
+import React from "react";
+import { GoogleMap, Marker, InfoWindow } from "@react-google-maps/api";
+import PropTypes from "prop-types";
 
 // Create Map component at the very top to render the entire map once
-const Map = withGoogleMap(props => (
-  <GoogleMap defaultCenter={{lat: 40.73971790095217, lng: -74.01005037971146}} defaultZoom={13}>
-    {props.markers.map(marker => (
-      <Marker key={marker.id} title={marker.title} position={marker.position}
-        animation={marker.animation} visible={marker.isVisible}
-        onClick={() => props.onToggleInfoWindow(marker)}>
-        {marker.showInfo && <InfoWindow onCloseClick={() => props.onToggleInfoWindow(marker)}>
-          {marker.url ? (
-            <div key={marker.id} className="marker-window">
-              <a className="marker-url" href={marker.url}>{marker.title}</a>
-              <img className="marker-image" src={marker.image} alt={marker.title} tabIndex={0}/>
-              <a className="marker-yelp" href="https://www.yelp.com">Yelp Reviews</a>
-              {marker.reviews ? (
-                marker.reviews.map(review => (
-                  // An alternative to creating another div child
-                  <React.Fragment key={review.id}>
-                    <h3 className="marker-rating" tabIndex={0}>Rating: {review.rating}</h3>
-                    <p className="marker-review" tabIndex={0}>{review.text}</p>
-                  </React.Fragment>
-                ))
-              ) : (
-                <h3 className="error-message" tabIndex={0}>Error! Yelp reviews were unable to load.</h3>
-              )}
-            </div>
-          ) : (
-            <h3 className="error-message" tabIndex={0}>
-              Error! Yelp could not find the location selected.
-            </h3>
-          )}
-        </InfoWindow>}
-      </Marker>
-    ))}
-  </GoogleMap>
-));
+// const Map = withGoogleMap((props) => (
+//   <GoogleMap center={center} zoom={10} onLoad={onLoad} onUnmount={onUnmount}>
+//     {props.markers.map((marker) => (
+//       <Marker
+//         key={marker.id}
+//         title={marker.title}
+//         position={marker.position}
+//         animation={marker.animation}
+//         visible={marker.isVisible}
+//         onClick={() => props.onToggleInfoWindow(marker)}
+//       >
+//         {marker.showInfo && (
+//           <InfoWindow onCloseClick={() => props.onToggleInfoWindow(marker)}>
+//             {marker.url ? (
+//               <div key={marker.id} className="marker-window">
+//                 <a className="marker-url" href={marker.url}>
+//                   {marker.title}
+//                 </a>
+//                 <img
+//                   className="marker-image"
+//                   src={marker.image}
+//                   alt={marker.title}
+//                   tabIndex={0}
+//                 />
+//                 <a className="marker-yelp" href="https://www.yelp.com">
+//                   Yelp Reviews
+//                 </a>
+//                 {marker.reviews ? (
+//                   marker.reviews.map((review) => (
+//                     // An alternative to creating another div child
+//                     <React.Fragment key={review.id}>
+//                       <h3 className="marker-rating" tabIndex={0}>
+//                         Rating: {review.rating}
+//                       </h3>
+//                       <p className="marker-review" tabIndex={0}>
+//                         {review.text}
+//                       </p>
+//                     </React.Fragment>
+//                   ))
+//                 ) : (
+//                   <h3 className="error-message" tabIndex={0}>
+//                     Error! Yelp reviews were unable to load.
+//                   </h3>
+//                 )}
+//               </div>
+//             ) : (
+//               <h3 className="error-message" tabIndex={0}>
+//                 Error! Yelp could not find the location selected.
+//               </h3>
+//             )}
+//           </InfoWindow>
+//         )}
+//       </Marker>
+//     ))}
+//   </GoogleMap>
+// ));
 
 // Alert when Google Maps authentication fails
 window.gm_authFailure = () => {
-  alert('Google Maps authentication failed.');
+  alert("Google Maps authentication failed.");
 };
 
-class NeighborhoodMap extends Component {
-	// Ensure props are the right variable type
-	static propTypes = {
-		markers: PropTypes.array.isRequired,
-		onToggleInfoWindow: PropTypes.func.isRequired
-	};
+const NeighborhoodMap = ({ markers, onToggleInfoWindow }) => {
+  // Ensure props are the right variable type
+  // static propTypes = {
+  //   markers: PropTypes.array.isRequired,
+  //   onToggleInfoWindow: PropTypes.func.isRequired,
+  // };
+  const [map, setMap] = React.useState(null);
 
-	render() {
-		const {markers, onToggleInfoWindow} = this.props;
+  const onLoad = React.useCallback(function callback(map) {
+    // const bounds = new window.google.maps.LatLngBounds(center);
+    // map.fitBounds(bounds);
+    setMap(map);
+  }, []);
 
-		return (
-			<Map loadingElement={<div className="map-loading"/>}
-				containerElement={<div className="map-container"/>}
-        mapElement={<div className="map" role="application" tabIndex={0}
-          aria-label="Map of neighborhood"/>}
-        markers={markers} onToggleInfoWindow={onToggleInfoWindow}/>
-		);
-	}
-}
+  const onUnmount = React.useCallback(function callback(map) {
+    setMap(null);
+  }, []);
+
+  const center = { lat: 40.73971790095217, lng: -74.01005037971146 };
+
+  return (
+    // <Map
+    //   loadingElement={<div className="map-loading" />}
+    //   containerElement={<div className="map-container" />}
+    //   mapElement={
+    //     <div
+    //       className="map"
+    //       role="application"
+    //       tabIndex={0}
+    //       aria-label="Map of neighborhood"
+    //     />
+    //   }
+    //   markers={markers}
+    //   onToggleInfoWindow={onToggleInfoWindow}
+    // />
+    <div className="map-container">
+      <GoogleMap
+        mapContainerClassName="map"
+        center={center}
+        zoom={13}
+        onLoad={onLoad}
+        onUnmount={onUnmount}
+      >
+        {markers.map((marker) => (
+          <Marker
+            key={marker.id}
+            title={marker.title}
+            position={marker.position}
+            animation={marker.animation}
+            visible={marker.isVisible}
+            onClick={() => onToggleInfoWindow(marker)}
+          >
+            {marker.showInfo && (
+              <InfoWindow onCloseClick={() => onToggleInfoWindow(marker)}>
+                {marker.url ? (
+                  <div key={marker.id} className="marker-window">
+                    <a className="marker-url" href={marker.url}>
+                      {marker.title}
+                    </a>
+                    <img
+                      className="marker-image"
+                      src={marker.image}
+                      alt={marker.title}
+                    />
+                    <a className="marker-yelp" href="https://www.yelp.com">
+                      Yelp Reviews
+                    </a>
+                    {marker.reviews ? (
+                      marker.reviews.map((review) => (
+                        // An alternative to creating another div child
+                        <React.Fragment key={review.id}>
+                          <h3 className="marker-rating">
+                            Rating: {review.rating}
+                          </h3>
+                          <p className="marker-review">{review.text}</p>
+                        </React.Fragment>
+                      ))
+                    ) : (
+                      <h3 className="error-message">
+                        Error! Yelp reviews were unable to load.
+                      </h3>
+                    )}
+                  </div>
+                ) : (
+                  <h3 className="error-message">
+                    Error! Yelp could not find the location selected.
+                  </h3>
+                )}
+              </InfoWindow>
+            )}
+          </Marker>
+        ))}
+      </GoogleMap>
+    </div>
+  );
+};
 
 export default NeighborhoodMap;
